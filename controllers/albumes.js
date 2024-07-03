c578cfae4805962deefb45fbf9432034b25ca42a
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2024-07-03 15:12:28 -0300

    albumes ready 100%

--- controllers/albumes.js
+++ controllers/albumes.js
@@ -2,13 +2,14 @@ import { conn } from "../db.js";
 
 const getAlbumes = async (_, res) => {
 
-        const [rows, fields] = await conn.query 
-        (`SELECT albumes.id, albumes.nombre, artistas.nombre AS nombre_artista FROM albumes
-            JOIN artistas on artistas.id = albumes.artista`);
-        res.json(rows);
+    const [rows, fields] = await conn.query 
+    (`SELECT albumes.id, albumes.nombre, artistas.nombre AS nombre_artista FROM albumes
+    JOIN artistas on artistas.id = albumes.artista`);
+    res.json(rows);
 };
 
 const getAlbum = async (req, res) => {
+    
     const id = req.params.id;
     const [rows, fields] = await conn.query 
     (`SELECT albumes.id, albumes.nombre, artistas.nombre AS nombre_artista from albumes
@@ -18,6 +19,7 @@ const getAlbum = async (req, res) => {
 };
 
 const createAlbum = async (req, res) => {
+    
     const nombre = req.body.nombre;
     const artista = req.body.artista;
     const [rows, fields] = await conn.query
@@ -25,11 +27,11 @@ const createAlbum = async (req, res) => {
     res.json({
         nombre: nombre,
         artista: artista
-      });
-    
+        });
 };
 
 const updateAlbum = async (req, res) => {
+    
     const id = req.params.id;
     const nombre = req.body.nombre;
     const artista = req.body.artista;
@@ -50,13 +52,15 @@ const deleteAlbum = async (req, res) => {
 };
 
 const getCancionesByAlbum = async (req, res) => {
-    const [rows, fields] = await conn.query
-    (`SELECT canciones.id, canciones.nombre, artistas.nombre AS nombre_artista, albumes.nombre AS nombre_album, canciones.duracion, canciones.reproducciones  
-    FROM canciones
-    JOIN albumes on canciones.album = albumes.id
-    JOIN artistas on albumes.artista = artistas.id
-    WHERE albumes.id = ?`,[id]); //CHEQUEAR
-    res.json(rows);
+    
+    const id = req.params.id;
+    const [rows, fields] = await conn.query(`
+        SELECT canciones.id, canciones.nombre, artistas.nombre AS nombre_artista, albumes.nombre AS nombre_album, canciones.duracion, canciones.reproducciones  
+        FROM canciones
+        JOIN albumes on canciones.album = albumes.id
+        JOIN artistas on albumes.artista = artistas.id
+        WHERE albumes.id = ?`, [id]);
+        res.json(rows);
 };
 
 const albumes = {
